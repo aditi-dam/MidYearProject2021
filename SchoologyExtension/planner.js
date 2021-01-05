4f729ae2ae544068546ff9fbab678cd76e37264f
Satwika <satwika.vemuri@gmail.com>
2021-01-05 14:26:40 -0500

    Working between files

--- SchoologyExtension/planner.js
+++ SchoologyExtension/planner.js
@@ -1,5 +1,14 @@
-function deleteOverdue() {
-    $('div.overdue-submissions.overdue-submissions-wrapper')
-        .remove();
-}
-deleteOverdue();
+function addTasks() {
+    $('div#overdue-submissions.overdue-submissions.overdue-submissions-wrapper').remove();
+        //$('div.upcoming-event.course-event').remove();
+    var upcoming = $('div.upcoming-events.upcoming-events-wrapper.sEventUpcoming-processed');
+    var list = $('div.upcoming-list');
+    var event = $('div.upcoming-event.course-event');
+    var x = event.children()[1].innerText
+    
+   console.log(x);
+    
+  
+  }
+
+  addTasks();
--- SchoologyExtension/popup.js
+++ SchoologyExtension/popup.js
@@ -1,14 +1,14 @@
-//document object represents our popup
-
 
 document.addEventListener('DOMContentLoaded', function() { //calls the below when the initial HTML document is completely loaded
-   
+  
+
   var htmlRoot = document.children[0];
   var body = htmlRoot.children[1]
   var firstP = body.children[1];
-  
-  var tasks = "hello";
-
-  firstP.innerText = tasks;
+  firstP.innerText = "hello";
   
 }, false);
+
+
+
+
