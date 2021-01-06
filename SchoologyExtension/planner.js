21873a1ea4bb27d42502b8482d1c65d143a65e3e
aaacoder <aditi.dam@gmail.com>
2021-01-06 13:03:45 -0500

    found a weird js import statement and added it. doesn't work tho

--- SchoologyExtension/planner.js
+++ SchoologyExtension/planner.js
@@ -1,10 +1,12 @@
-function addTasks() {
+export function addTasks() {
     $('div#overdue-submissions.overdue-submissions.overdue-submissions-wrapper').remove();
         //$('div.upcoming-event.course-event').remove();
     var upcoming = $('div.upcoming-events.upcoming-events-wrapper.sEventUpcoming-processed');
     var list = $('div.upcoming-list');
     var event = $('div.upcoming-event.course-event');
-    var x = event.children()[1].innerText
+    var x = event.children()[1].innerText;
+
+    return x;
  
   }
 
--- SchoologyExtension/popup.js
+++ SchoologyExtension/popup.js
@@ -1,10 +1,10 @@
+import { addTasks } from './planner.js'
+
 document.addEventListener('DOMContentLoaded', function() { //calls the below when the initial HTML document is completely loaded
   
     var htmlRoot = document.children[0];
     var body = htmlRoot.children[1]
     var firstP = body.children[1];
-    firstP.innerText = "hello";
-    
-  
+    firstP.innerText = planner.addTasks();
 
 }, false);
