591d6878c570f52df7e95666ed4d88cd9fd20b39
Satwika <satwika.vemuri@gmail.com>
2021-01-06 14:55:03 -0500

    addTasks is called in popup (remove doesn't work)

--- SchoologyExtension/popup.js
+++ SchoologyExtension/popup.js
@@ -1,10 +1,27 @@
-import { addTasks } from './planner.js'
 
 document.addEventListener('DOMContentLoaded', function() { //calls the below when the initial HTML document is completely loaded
   
     var htmlRoot = document.children[0];
     var body = htmlRoot.children[1]
     var firstP = body.children[1];
-    firstP.innerText = planner.addTasks();
+    firstP.innerText = "hello";
+
+    addTasks();
+    console.trace();
 
 }, false);
+
+function addTasks() {
+    $('div#overdue-submissions.overdue-submissions.overdue-submissions-wrapper').remove();
+        //$('div.upcoming-event.course-event').remove();
+    /*
+    var upcoming = $('div.upcoming-events.upcoming-events-wrapper.sEventUpcoming-processed');
+    var list = $('div.upcoming-list');
+    var event = $('div.upcoming-event.course-event');
+    var x = event.children()[1].innerText;
+
+    return x;
+    */
+ 
+  }
+
