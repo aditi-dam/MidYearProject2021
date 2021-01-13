b3ea8017623bc148595ed19295501dac9090ec27
aaacoder <aditi.dam@gmail.com>
2021-01-13 13:05:03 -0500

    Changing stuff

--- SchoologyExtension/popup.js
+++ SchoologyExtension/popup.js
@@ -9,11 +9,18 @@ document.addEventListener('DOMContentLoaded', function() { //calls when the DOM 
 function addTask(){ 
   //Aditi can you figure out how to display what was typed in the entry box. 
   //Try displaying it in the "taskone" for now
+  
+  var inputValue = document.getElementbyId("taskone").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  document.getElementById("myList").appendChild(li);
 
 
   //document.getElementById("taskone").innerHTML = document.getElementByName("task")[0].value;
-  document.getElementById("taskone").innerHTML = "hello";
+  //document.getElementById("taskone").innerHTML = "hello";
 }
+
+
 
 // Once the DOM is ready...
 
