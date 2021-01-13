676a50561f91b10d74440237d621aae1a89fae62
Satwika <satwika.vemuri@gmail.com>
2021-01-13 13:54:24 -0500

    can get input from entry box

--- SchoologyExtension/background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener((msg, sender) => {
-    // First, validate the message's structure.
-    if ((msg.from === 'content') && (msg.subject === 'showPageAction')) {
-      // Enable the page-action for the requesting tab.
-      console.log("I'm here");
-      chrome.pageAction.show(sender.tab.id);
-      console.log("I did the show stuff");
-    }
-});
