20c9ede7458d7713cd7dbb357b8271c20febc7bf
Satwika <satwika.vemuri@gmail.com>
2021-01-14 16:47:50 -0500

    some storage stuff?

--- SchoologyExtension/popup.js
+++ SchoologyExtension/popup.js
@@ -14,20 +14,34 @@ document.addEventListener('click', function(){
 
 });
 */
+//https://stackoverflow.com/questions/40286947/how-to-access-multiple-keys-from-chrome-extensions-storage-api
 
 document.addEventListener('DOMContentLoaded', function(){
   var button = document.getElementById("task");
-  button.addEventListener("click", function(){
-    var text = document.getElementById("myInput").value
+  chrome.storage.local.get(['task'], function(data){
+
+    var text = data.task;
+
     var tag = document.createElement("li");
     var text = document.createTextNode(text);
     tag.appendChild(text);
     var element = document.getElementById("myList");
     element.appendChild(tag);
-    click = false;
+  });
+
+  button.addEventListener("click", function(){
+    var text = document.getElementById("myInput").value
+    chrome.storage.local.set({'task': text});
+
+    var tag = document.createElement("li");
+    var text = document.createTextNode(text);
+    tag.appendChild(text);
+    var element = document.getElementById("myList");
+    element.appendChild(tag);
   });
 
 });
+
 
 
 
