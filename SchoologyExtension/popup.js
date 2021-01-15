ef2b01e1933e4fa5eec85df955f6cf3f8805f5ba
Satwika <satwika.vemuri@gmail.com>
2021-01-15 14:38:05 -0500

    STORAGE WORKS!!! But all on one task. BUT STILL!!

--- SchoologyExtension/popup.js
+++ SchoologyExtension/popup.js
@@ -2,8 +2,6 @@
 
 //https://stackoverflow.com/questions/40286947/how-to-access-multiple-keys-from-chrome-extensions-storage-api
 
-var allTaskKeys = []; 
-var count = 1;
 
 document.addEventListener('DOMContentLoaded', function(){
   
@@ -14,35 +12,33 @@ document.addEventListener('DOMContentLoaded', function(){
   allTaskKeys.push('task2')
   */
 
-  var defaultValue = [];
-  chrome.storage.sync.get({taskKeys: defaultValue}, function(data) {
-    // data.taskKeys will be either the stored value, or defaultValue if nothing is set
-    chrome.storage.sync.set({taskKeys: data.taskKeys}, function() {
-      // The value is now stored, so you don't have to do this again
-      allTaskKeys = data.taskKeys;
-    });
-  });
 
 
   var button = document.getElementById("task");
 
-  for(i = 0; i < allTaskKeys.length; i++){
-    var taskKey = allTaskKeys[i]
-    chrome.storage.local.get([taskKey], function(data){
-    var text = data.task1
-      //var text = data.taskKey;
-    addTask(text);
+
+  chrome.storage.sync.get("task", function(data){
+      
+      var text = data.task;
+      addTask(text);
+      console.log(text);
     
-    });
-  }
+  });
+
 
   button.addEventListener("click", function(){
-    var text = document.getElementById("myInput").value 
-    addTask(text);
+    var newElement = document.getElementById("myInput").value 
+    addTask(newElement);
+
+    var text = "";
+    var ul = document.getElementById("myList");
+    var items = ul.getElementsByTagName("li");
     
-    var taskKey = 'task1';
-    allTaskKeys.push(taskKey);
-    chrome.storage.sync.set({taskKeys: allTaskKeys}, function() {});
+    for (var i = 0; i < items.length; ++i) {
+      text += items[i].innerHTML 
+    }
+    chrome.storage.sync.set({"task": text});
+    
     
   });
 
